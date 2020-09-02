1b73c959ab66260610607a69cfd0867d624f1540
ひろむ <48143196+hiromuaraki@users.noreply.github.com>
2020-09-03 00:54:21 +0900

    取得文追加

--- scraping.py
+++ scraping.py
@@ -1,9 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 target_url = "https://www.yahoo.co.jp/"
 r = requests.get(target_url)
 soup = BeautifulSoup(r.text, 'lxml')
-
+soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
 soup.prettify()
 
