76bdb1b81766030eac2acf394d584a2397fa9c73
ひろむ <48143196+hiromuaraki@users.noreply.github.com>
2020-09-05 11:53:29 +0900

    模範解答ロジック追加 勉強用

--- shift-only.py
+++ shift-only.py
@@ -1,3 +1,4 @@
+#自分のロジック
 n = int(input())
 k = list(map(int, input().split()))
 cnt = r =  0
@@ -7,3 +8,15 @@ if k[0] % 2 == 0:
         r /= 2
         cnt += 1
 print(cnt)
+
+#他の人のロジック
+n = input()
+a = list(map(int, input().split()))
+#無限大の数を取得
+ans = float('inf')
+#[入力値の要素]数文繰り返す
+for i in a:
+    #10進数→2進数へ変換　例:8→0b1000→6
+    #rfind('1')が最初に登場するインデックス番号
+  ans = min(ans, len(bin(i)) - bin(i).rfind('1') - 1)
+print(ans)
